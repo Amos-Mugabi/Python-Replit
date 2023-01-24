a24bbbfa5bfa5135399b16724885e215d9a3dc74
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-01-24 21:58:45 +0300

    Add files via upload

---
+++ Replit100-Day-2.py
@@ -0,0 +1,15 @@
+input("What is your name?:")
+input("What is your favourite food:")
+input("What is your favourite music?")
+input("Where do you live?:")
+print()
+
+print("""
+You are
+Mark
+You`re probably hungry for
+Burritos
+and you`re definitely getting your groove on to 
+My man, Rick Astley
+living in the amazing
+california""")
