a77da22d3a427c4dfbeed00dbc9a2d68bfbbd1be
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-01-25 10:43:55 +0300

    Add files via upload

---
+++ replit day 3.py
@@ -0,0 +1,10 @@
+food = input("Name a food > ")
+plant = input("Name a type of plant > ")
+method = input("Name a method of cooking > ")
+burned = input("What word desrcibes burned food? > ")
+DIY = input("Name a DIY item > ")
+
+print()
+
+
+print( "MENU",  method, food, "with", burned, plant, "on a bed of", DIY )
