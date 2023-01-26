f78de38042bcf025d0279b9898fd5bca1fbd59ad
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-01-26 16:39:27 +0300

    Add files via upload

---
+++ Replit100-Day-4.py
@@ -0,0 +1,12 @@
+print("Welcome to your adventure simulator. I am going to ask you a bunch of questions and then create an epic story with you as the star!")
+print()
+
+name = input("What is your name? ")
+enemy = input("What is your worst enemy`s name? ")
+superpower = input("What is your superpower? ")
+
+live = input("Where do you live? ")
+food = input("What is your favorite food? ")
+print()
+
+print("Hello", name, "Your ability to ", superpower, "will make sure you never have to look at ", enemy, "again.", "Go eat", food, "as you walk down the streets of", live, "and use", superpower, "for good and not evil!"  )
