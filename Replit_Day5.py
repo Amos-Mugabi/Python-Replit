ae0ca1940a0839ccd216702fcc2e5ced55bf19ac
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-01-30 14:11:13 +0300

    Add files via upload

---
+++ Replit_Day5.py
@@ -0,0 +1,21 @@
+print("Marvel Movie Character Creator")
+print("--")
+print()
+hang = input("Do you like 'hanging around'?: ")
+if hang == "yes":
+  print("You are not spider-man")
+else:
+  print("Then you`re not spider-man")
+
+grave = input("Do you have a 'gravelly' voice?: ")
+if grave == "yes":
+  print("Aww, then you`re  korg")
+else:
+  print("Then you`re not korg")
+
+marvel = input("Do you often feel 'Marvelous'?: ")
+if marvel == "yes":
+  print("Aha! You`re Captain Marvel! Hi!")
+else:
+  print("Then you`re not Captain Marvel")
+
