ef193d645939a77b6effc82f0377c24bcef4dc5c
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-02-16 16:10:24 +0300

    Add files via upload

---
+++ Replitday6.py
@@ -0,0 +1,19 @@
+print(""""MY LOGIN SYSTEM
+          ++++++++++++++""")
+
+
+username = input("Username> ")
+password = input("Password> ")
+
+
+if username == "Peter" and password == "pet123":
+  print("Why hello there Peter, what a lovely accent you have,  you could have charmed your way in here even without a password. ")
+  print()
+  print("Have a great day.")
+
+else:
+  print("Incorrect username or password. Try again")
+
+
+
+
