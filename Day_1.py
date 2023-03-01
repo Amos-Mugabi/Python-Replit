9391754c5b5a1b50bf090d0f06a57214d675f350
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-03-01 09:26:50 +0300

    Add files via upload

---
+++ Day_1.py
@@ -0,0 +1,12 @@
+# Day 1
+
+print("""
+Amos
+
+January 18th 2023
+
+I am signing up for Replit`s 100 days of Python challenge! I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day. I`ll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be.No excuses for not coding from the middle of a field! "
+
+I am feeling  happy.
+You can follow my progress atreplit.com/@Amos-Mugabi
+""")
