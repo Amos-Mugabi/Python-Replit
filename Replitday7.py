97858230389f63aad7e818e38492b3973594bddc
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-03-02 19:48:31 +0300

    Add files via upload

---
+++ Replitday7.py
@@ -0,0 +1,22 @@
+# day 7
+print("Fake Fan Finder 😎")
+anime = input("What`s your favourite Anime? ")
+if anime == "one piece":
+    print("oh really?!")
+else:
+    print("Ugh boring")
+    character = input("Name me any of the characters?")
+    if character == "Nami":
+        print("You got that by chance.")
+    else:
+        print("wrong")
+        job = input("Okay then, what is her job on the ship? ")
+        if job == "Navigator":
+            print("Hmph!")
+        else:
+            print("You missed it.")
+        bounty = input("What was her first bounty then?")
+        if bounty == "Ummm...":
+            print("See! FAKE One piece fan!")
+        else:
+            print("Nope Try again")
