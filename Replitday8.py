2983f9b67bb02ed9c74ce83ec7726dbd21ff8ae2
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-03-03 19:04:08 +0300

    Add files via upload

---
+++ Replitday8.py
@@ -0,0 +1,26 @@
+# Day 8
+print("Positive Affirmations Generator")
+print()
+
+name = input("Who are you? ")
+if name == "John":
+    print("Good to know you John.")
+    day = input("What is the day of the week? ")
+    if day == "Monday" or day == "monday":
+        print("Opportunity Monday")
+
+        if day == "Tuesday" or day == "tuesday":
+            print("Happy Tuesday")
+        if day == "Wednesday" or day == "wednesday":
+            print("Lovely Wednesday")
+        if day == "Thursday" or day == "thursday":
+            print("Joyful Thursday")
+        if day == "Friday" or day == "friday":
+            print("Energy Friday")
+    passion = input("What are your passions?")
+    if passion == "software" or passion == "engineering":
+        print("""Great to hear this!"
+Hey John, Do not give up. One day you will be the greatest scientist the world has ever seen. I believe in you.""")
+      
+else:
+        print("Sorry, who is this?")
