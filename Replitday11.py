6f9dc67d1ab7379fa2bcdf61b9dc759fe37a740b
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-03-14 16:00:07 +0300

    Add files via upload

---
+++ Replitday11.py
@@ -0,0 +1,13 @@
+#seconds calculator
+
+totaldays = int(input("How many days are in this year?"))
+day = 24 * 60 * 60
+
+year = 365 * day
+leapyear = 366 * day
+
+if totaldays == 366:
+
+    print("There are ", leapyear, "seconds", "in this year")
+else:
+    print("There are ", year, "seconds", "in this year")
