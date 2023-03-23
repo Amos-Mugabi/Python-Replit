c02442a3624f7854a880d71029f05fd16cae906c
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-03-23 13:24:29 +0300

    Update Replitday6.py

--- Replitday6.py
+++ Replitday6.py
@@ -5,7 +5,6 @@ print(""""MY LOGIN SYSTEM
 username = input("Username> ")
 password = input("Password> ")
 
-
 if username == "Peter" and password == "pet123":
   print("Why hello there Peter, what a lovely accent you have,  you could have charmed your way in here even without a password. ")
   print()
@@ -13,7 +12,3 @@ if username == "Peter" and password == "pet123":
 
 else:
   print("Incorrect username or password. Try again")
-
-
-
-
