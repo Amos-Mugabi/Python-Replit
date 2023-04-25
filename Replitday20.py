1ee7b840db744bd6ebc8719abf9d2e185b524751
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-04-25 16:54:30 +0300

    Add files via upload

---
+++ Replitday20.py
@@ -0,0 +1,10 @@
+# For Loop with increments
+print("List Generator")
+print()
+start = int(input("Start at: "))
+end = int(input("End before: "))
+increment = int(input("Increment between values: "))
+i = 0
+for i in range(start, end, increment):
+
+  print(i)
